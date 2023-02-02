20225181727157a72b0bf89c200e5184043121ce
JulienForesier <forestierjulien3@gmail.com>
2023-02-02 18:09:49 +0100

    blocage étape 8

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,32 +1,35 @@
 const cartItems = document.getElementById("cart__items");
 let basket = JSON.parse(localStorage.getItem("basket"));
 
-console.log(basket);
-
-async function fetchBasket(product) {
+async function fetchProduct(product) {
   await fetch(`http://localhost:3000/api/products/${product.id}`)
-    .then((res) => res.json)
-    .then((data) => (product = data));
+    .then((res) => res.json())
+    .then((data) => (produit = data));
+  console.log(produit);
 }
 
 function basketDisplay(basket) {
-  cartItems.innerHTML = basket.map(
-    (product) =>
-      `
-    <article class="cart__item" data-id="${product.ID}" data-color="${product.color}">
+  let produit = {};
+  cartItems.innerHTML = basket.map((product) => {
+    setTimeout(() => {
+      fetchProduct(product);
+    }, 10);
+    console.log(produit);
+    return `
+    <article class="cart__item" data-id="${product.ID}" data-color=${product.color}>
      <div class="cart__item__img">
-                  <img src="" alt="">
+                  <img src=${produit.imgageUrl} alt=${produit.altTxt}>
                 </div>
                 <div class="cart__item__content">
                   <div class="cart__item__content__description">
-                    <h2></h2>
-                    <p>${product.color}</p>
-                    <p></p>
+                    <h2>${produit.name}</h2>
+                    <p>${produit.price}</p>
+                    <p>${produit.price}</p>
                   </div>
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
                       <p>Qté : ${product.quantity}</p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${product.quantity}>
                     </div>
                     <div class="cart__item__content__settings__delete">
                       <p class="deleteItem">Supprimer</p>
@@ -34,7 +37,8 @@ function basketDisplay(basket) {
                   </div>
                 </div>
     </article>
-    `
-  );
+    `;
+  });
 }
+
 basketDisplay(basket);
